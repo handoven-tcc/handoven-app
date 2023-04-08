1faf1e48f507dd9da10387ffaa7023153faf8b39
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-04-08 18:52:51 -0300

    Revert "2"
    This reverts commit 79a8c1354ed68100b2410914a71226d10ccb5cde.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text, View } from '../../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>TESTE 2</Text>
+      <Text style={styles.title}>TESTE 1</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
