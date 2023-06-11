5fd424ac771b14f893e06f81bd5e8228ac3eec8b
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-06-10 21:56:23 -0300

    Adicionado library temp

---
+++ temp/src/lib/temp.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import {ComponentsModule} from './components/components.module';
+import {ToolsModule} from './tools/tools.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    ComponentsModule,
+    ToolsModule
+  ],
+  exports: [
+    ComponentsModule,
+    ToolsModule
+  ]
+})
+export class TempModule { }
---
+++ temp/src/lib/tools/tools.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+
+const directives: any = [
+
+];
+
+const pipes: any = [
+
+];
+
+@NgModule({
+	declarations: [
+			...pipes
+	],
+	exports: [
+			...pipes
+	],
+  imports: [
+  ]
+})
+export class ToolsModule { }
