f41724abac7bba0acf3b87102e1aeb47bcfc905b
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-06-10 21:54:45 -0300

    Adicionado estrutura base de ionic angular cordova

---
+++ src/app/tab1/tab1.module.ts
@@ -0,0 +1,24 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ButtonModule } from 'primeng/button';
+
+import { Tab1Page } from './tab1.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { Tab1PageRoutingModule } from './tab1-routing.module';
+
+@NgModule({
+  imports: [
+    ButtonModule,
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ExploreContainerComponentModule,
+    Tab1PageRoutingModule
+  ],
+  declarations: [Tab1Page]
+})
+export class Tab1PageModule {}
