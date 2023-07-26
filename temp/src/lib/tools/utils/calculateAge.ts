ec5512f3325060799ef2d976890ac58d7ace7170
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-07-26 20:04:23 -0300

    feat: adicionado calculo de idade

---
+++ temp/src/lib/tools/utils/calculateAge.ts
@@ -0,0 +1,18 @@
+export function calculateAge(birthday: Date): number {
+  const now = new Date();
+  const birthDate = new Date(birthday);
+
+  const utcNow = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
+  const utcBirthDate = Date.UTC(
+    birthDate.getFullYear(),
+    birthDate.getMonth(),
+    birthDate.getDate()
+  );
+
+  const ageDifMs = utcNow - utcBirthDate;
+
+  const ageDate = new Date(ageDifMs);
+  const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+  return age;
+}
