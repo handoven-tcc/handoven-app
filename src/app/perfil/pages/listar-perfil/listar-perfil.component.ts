1e43dc5413c0ccc9d675855a9d5fab7a41a63e96
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-07-28 18:19:24 -0300

    style(APP-13): estilos de header e footer

---
+++ src/app/perfil/pages/listar-perfil/listar-perfil.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-perfil',
+  templateUrl: './listar-perfil.component.html',
+  styleUrls: ['./listar-perfil.component.scss'],
+})
+export class ListarPerfilComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
