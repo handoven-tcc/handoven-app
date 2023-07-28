2954418ffa256055b30016a42a05fc3a1e297fa2
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-07-28 18:19:33 -0300

    chore(APP-13): renomeando perfil interno para listar-perfil

--- src/app/perfil/perfil-routing.module.ts
+++ src/app/perfil/perfil-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from "@angular/router";
 import { PerfilComponent } from "./perfil.component";
 import { AdicionarIntegranteComponent } from "./pages/adicionar-integrante/adicionar-integrante.component";
 import { EditarPerfilComponent } from "./pages/editar-perfil/editar-perfil.component";
-import { PerfilInternoComponent } from "./pages/perfil-interno/perfil-interno.component";
 import { AuthGuard } from "../auth/guard/auth.guard";
+import { ListarPerfilComponent } from "./pages/listar-perfil/listar-perfil.component";
 
 const routes: Routes = [
   {
     path: "",
     component: PerfilComponent,
     children: [
-      { path: "", component: PerfilInternoComponent },
+      { path: "", component: ListarPerfilComponent },
       { path: "adicionar-integrante", component: AdicionarIntegranteComponent },
       { path: "editar-perfil/:perfilId", component: EditarPerfilComponent },
     ],
