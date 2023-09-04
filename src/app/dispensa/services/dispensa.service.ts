2cddb0ef0a9f81566870bd8ae8cb97bf420751c1
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-09-04 19:56:40 -0300

    feat: Adicionado hasUsuario na service de auth

--- src/app/dispensa/services/dispensa.service.ts
+++ src/app/dispensa/services/dispensa.service.ts
@@ -1,46 +1,60 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "../../../environments/environment";
-import { Observable, map } from "rxjs";
+import { Observable, map, of } from "rxjs";
 import {
   ProductRequest,
   DeleteProductRequest,
   GetProductByNameRequest,
   GetProductByIdRequest,
-  GetAllProductsRequest,
   ProductsResponse,
 } from "../models";
+import { AuthService } from "../../auth/services";
 
 @Injectable({
   providedIn: "root",
 })
 export class DispensaService {
+  private familiaId!: string;
+  private usuarioId!: string;
+
   private get url(): string {
     return `${environment.api}/products`;
   }
 
-  constructor(private http: HttpClient) // private storage: StorageService,
-  {}
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService // private storage: StorageService,
+  ) {
+    this.familiaId = this.authService.getFamiliaId();
+    this.usuarioId = this.authService.getUsuarioId();
+  }
 
-  getAllProducts(
-    request: GetAllProductsRequest
-  ): Observable<ProductsResponse[]> {
+  getAllProducts(): Observable<ProductsResponse[]> {
+    if (!this.authService.hasUsuario()) {
+      return of([] as ProductsResponse[]);
+    }
+
     return this.http
       .get(this.url, {
         headers: {
-          "X-HandOven-User": request.usuarioId,
-          "X-HandOven-Family": request.familiaId,
+          "X-HandOven-User": this.usuarioId,
+          "X-HandOven-Family": this.familiaId,
         },
       })
       .pipe(map((res: any) => res));
   }
 
   getProductById(request: GetProductByIdRequest): Observable<ProductsResponse> {
+    if (!this.authService.hasUsuario()) {
+      return of({} as ProductsResponse);
+    }
+
     return this.http
       .get(`${this.url}/${request.id}`, {
         headers: {
-          "X-HandOven-User": request.usuarioId,
-          "X-HandOven-Family": request.familiaId,
+          "X-HandOven-User": this.usuarioId,
+          "X-HandOven-Family": this.familiaId,
         },
       })
       .pipe(map((res: any) => res));
@@ -49,22 +63,30 @@ export class DispensaService {
   getProductsByName(
     request: GetProductByNameRequest
   ): Observable<ProductsResponse[]> {
+    if (!this.authService.hasUsuario()) {
+      return of([] as ProductsResponse[]);
+    }
+
     return this.http
-      .post(`${this.url}/name/familyId/${request.familiaId}`, request, {
+      .post(`${this.url}/name/familyId/${this.familiaId}`, request, {
         headers: {
-          "X-HandOven-User": request.usuarioId,
-          "X-HandOven-Family": request.familiaId,
+          "X-HandOven-User": this.usuarioId,
+          "X-HandOven-Family": this.familiaId,
         },
       })
       .pipe(map((res: any) => res));
   }
 
   postProduct(request: ProductRequest): Observable<ProductsResponse[]> {
+    if (!this.authService.hasUsuario()) {
+      return of([] as ProductsResponse[]);
+    }
+
     return this.http
       .post(this.url, request, {
         headers: {
-          "X-HandOven-User": request.usuarioId,
-          "X-HandOven-Family": request.familiaId,
+          "X-HandOven-User": this.usuarioId,
+          "X-HandOven-Family": this.familiaId,
           "X-handOven-Service": "false",
         },
       })
@@ -72,22 +94,30 @@ export class DispensaService {
   }
 
   putProductById(request: ProductRequest): Observable<ProductsResponse[]> {
+    if (!this.authService.hasUsuario()) {
+      return of([] as ProductsResponse[]);
+    }
+
     return this.http
       .put(`${this.url}/${request.id}`, request, {
         headers: {
-          "X-HandOven-User": request.usuarioId,
-          "X-HandOven-Family": request.familiaId,
+          "X-HandOven-User": this.usuarioId,
+          "X-HandOven-Family": this.familiaId,
         },
       })
       .pipe(map((res: any) => res));
   }
 
   deleteProductById(request: DeleteProductRequest): Observable<any> {
+    if (!this.authService.hasUsuario()) {
+      return of([] as ProductsResponse[]);
+    }
+
     return this.http
       .delete(`${this.url}/${request.id}`, {
         headers: {
-          "X-HandOven-User": request.usuarioId,
-          "X-HandOven-Family": request.familiaId,
+          "X-HandOven-User": this.usuarioId,
+          "X-HandOven-Family": this.familiaId,
           "X-handOven-Service": "false",
         },
       })
