ef945cb8b544cfcba2ae8b44e1b8169ea906d505
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-09-23 23:42:52 -0300

    feat: Adicionado mais receitas e adicionado tipagem

--- src/app/favoritos/models/response/receitas-response.model.ts
+++ src/app/favoritos/models/response/receitas-response.model.ts
@@ -1,11 +1,34 @@
+enum ReceitaCategoria {
+  Sobremesas = 1,
+  Saladas = 2,
+  Sopas = 3,
+  Omeletes = 4,
+  "Pratos Asiáticos" = 5,
+  "Pratos Brasileiros" = 6,
+  Risotos = 7,
+  Frangos = 8,
+  Massas = 9,
+  Peixes = 10,
+  Pizzas = 11,
+  Bebidas = 12,
+  Aperitivos = 13,
+  Vegetarianas = 14,
+}
+
 interface ReceitasResponse {
   name: string;
   favorited: boolean;
-  secao:
-    (string[]
-      | ({ name: string; quantity: string; notes: string; }
-      | { name: string; quantity: string; notes?: undefined; }
-      )[])[];
+  category: ReceitaCategoria;
+  section: {
+    ingredients: {
+      name: string,
+      quantity: string,
+      category: string,
+      notes: string,
+    }[],
+    prepareMode: [],
+    extras: []
+  };
 }
 
 export { type ReceitasResponse };
