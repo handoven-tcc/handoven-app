832ed7881e60015b4c4758ed8a4d4dd5230a5761
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-10-05 20:29:28 -0300

    feat: Alterado response da api plates Adicionado ingredients_unit_measure

--- src/app/receitas/models/response/receitas-response.model.ts
+++ src/app/receitas/models/response/receitas-response.model.ts
@@ -7,15 +7,16 @@ interface ReceitasResponse {
   favorited: boolean;
   category: ReceitaCategoria;
   section: {
-    ingredients: IngredientesReceita[],
-    prepare_mode: [],
-    extras: string[]
+    ingredients: IngredientesReceita[];
+    prepare_mode: [];
+    extras: string[];
   };
 }
 
 interface IngredientesReceita {
   ingredients_name: string;
-  ingredients_quantity: string;
+  ingredients_quantity: number;
+  ingredients_unit_measure: string;
   ingredients_category: ReceitaIngredienteCategoria;
   ingredients_notes: string;
 }
